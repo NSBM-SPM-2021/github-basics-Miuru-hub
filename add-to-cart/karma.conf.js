cd18e093447deacc0079fdd9b5105b36a0133147
miuruprabodhasnm@gmail.com <miuruprabodhasnm@gmail.com>
2021-10-14 12:41:06 +0530

    we

--- add-to-cart/karma.conf.js
+++ add-to-cart/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/add-to-cart'),
+      dir: require('path').join(__dirname, './coverage/add-to-cart'),mhj
       subdir: '.',
       reporters: [
         { type: 'html' },
